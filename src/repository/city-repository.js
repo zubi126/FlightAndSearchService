c6e3d96999ce3e7ffba6b9dd7ad2cb71bcb00244
ZUBAIR AHMAD <zubairahmadbhat920712@gmail.com>
2025-02-10 09:48:15 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,25 +1,69 @@
 const { City } =require('../models/index')
 
 class CityRepository {
-    async createCity ({ name }){
-        try {
-            const city = await City.create({name});
-            return city
-        } catch (error) {
-            throw {error};
-        }
-    }
 
-
-async deleteCity(cityid){
+async createCity({ name }){
     try {
-        await City.destroy({
-            where: {
-                id : cityid
+        const city = await City.create({
+            name
+        })
+        return city;
+    } catch (error) {
+        console.log("somthing went wrong in repo layer");
+        throw {error}
+        
+    }
+}
+
+async deleteCity(cityId){
+    try {
+        
+await City.destroy({
+    where: {
+        id: cityId
+    }
+   
+})
+return true
+
+    } catch (error) {
+        
+console.log("something went wrong in repository layer");
+
+throw {error}
+
+    }
+}
+
+
+async updateCity(cityId, data){
+    try {
+        const city = await City.update(data,{
+            where:{
+                id:cityId
             }
         })
+        return city;
     } catch (error) {
-        throw {error};
+        console.log("something went wrong in repository layer");
+
+throw {error}
+    }
+}
+
+
+
+
+
+
+async getCity(cityId){
+    try {
+        const city = await City.findByPk(cityId)
+        return City
+    } catch (error) {
+        console.log("something went wrong in repository layer");
+
+throw {error}
     }
 }
 
